524e85be5a9b82dff0a9dde0b7f8d4fd56bd8b34
Afikur Rahman <sudipto.ruet@gmail.com>
2017-05-21 18:10:27 +0600

    outlet activity added

---
+++ app/src/main/java/app/salesforce/gnt/com/salesforce/OutletActivity.java
@@ -0,0 +1,13 @@
+package app.salesforce.gnt.com.salesforce;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OutletActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_outlet);
+    }
+}
---
+++ app/src/main/java/app/salesforce/gnt/com/salesforce/SERVICE_TYPE.java
@@ -0,0 +1,8 @@
+package app.salesforce.gnt.com.salesforce;
+
+/**
+ * Created by PC-05 on 5/21/2017.
+ */
+
+public enum SERVICE_TYPE {
+}
